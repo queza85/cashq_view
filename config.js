2cfbe1bfb06ae6c039c28513679d2799e01eb91e
Joe <queza85@gmail.com>
2017-07-30 23:27:44 +1200

    init project

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '1217759775000669',
+  clientSecret: '269a5749aebf4633e728ef57fe9e3c05',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '139487571250-gjsrcrd1bpjrtrmi4iun3porvj3o2mnk.apps.googleusercontent.com',
+  clientSecret: '_EeoyMZ_sC7fQRkhkyQ8_Dlb',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/auth/redirect', function (req, res) {
+  var qs = req._parsedUrl.query;
+	console.log(qs);
+console.log(process.env.NODE_ENV);
+//  if (process.env.NODE_ENV === 'development') {
+    res.redirect('exp://ge-cha.notbrent.app.exp.direct:80/+redirect/?' + qs);
+//  } else {
+//    res.redirect('exp://exp.host/@community/with-facebook-auth/+redirect/?' + qs);
+//  }
+});
+
+app.get('/auth/facebook', function (req, res) {
+  res.sendFile('facebook.html', {root: __dirname });
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log('앱은 http://%s:%s 에서 작동 중입니다.', host, port);
+});
+
